afdad22027ccfcdd6bc5c06b67ae7bcb222a52e2
Sn0wCrack <442287+Sn0wCrack@users.noreply.github.com>
2025-01-11 08:37:36 +1100

    feat: add embed check to misskey
    similar to twitter, wait for a small period to check if an embed was made first

--- SaucyBot/Site/Misskey.cs
+++ SaucyBot/Site/Misskey.cs
@@ -14,13 +14,16 @@ public sealed class Misskey : BaseSite
     protected override Color Color => new(0x85B300);
 
     private readonly ILogger<Misskey> _logger;
+    private readonly IConfiguration _configuration;
     private readonly IMisskeyClient _client;
 
     public Misskey(
         ILogger<Misskey> logger,
+        IConfiguration configuration,
         IMisskeyClient client
     ) {
         _logger = logger;
+        _configuration = configuration;
         _client = client;
 
         var domains = new List<string> { "misskey.io", "misskey.design", "oekakiskey.com" }
@@ -38,12 +41,28 @@ public sealed class Misskey : BaseSite
         var id = match.Groups["id"].Value;
         
         var note = await _client.ShowNote(url, id);
+
         if (note is null)
         {
             return null;
         }
-        
-        if (!ShouldEmbed(note))
+
+        var hasEmbed = false;
+
+        // If we have a message attached, we need to wait a bit for Discord to process the embed,
+        // we when need to refresh the message and see if an embed has been added in that time.
+        if (message is not null)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(_configuration.GetSection("Sites:Misskey:Delay").Get<double>()));
+
+            // NOTE: Discord.NET works a little interestingly, basically when a message updates the Bot learns of this change
+            // and then proceeds to update its internal cache, so while we're waiting around it should update the message cache
+            // automatically, so there's no need to refresh the message object.
+
+            hasEmbed = message.Embeds.Count != 0;
+        }
+
+        if (hasEmbed && !ShouldEmbed(note))
         {
             return null;
         }
